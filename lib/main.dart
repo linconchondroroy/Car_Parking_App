5638f74bfe54d42cf534c27ec32337f804e09ce9
Lincon Chondro Roy <linconchondroroy@gmail.com>
2025-11-04 19:39:05 +0600

    Car Parking App

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:car_parking_app/views/welcome_screen.dart';
+import 'package:car_parking_app/views/Welcome%20Screen/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'views/Onboarding Screen/onboarding_screen_one.dart';
 
 void main(){
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
+      home: OnboardingScreenOne(),
     );
   }
 }
