13ad6ef3b48f18b69e3c339abd36df358a04c19b
Lincon Chondro Roy <linconchondroroy@gmail.com>
2025-11-04 18:17:42 +0600

    Car Parking App

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF1976D2);
+  static const secondary = Color(0xFFFFC107);
+  static const background = Color(0xFF23335F);
+  static const textPrimary = Color(0xFFFFFFFF);
+  static const textSecondary = Color(0xFF18191B);
+  static const third = Color(0xFF191820);
+  static const buttonPrimary = Color(0xFFE8505B);
+  static const buttonSecondary = Color(0xFF);
+
+}
