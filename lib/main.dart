daed074912754a2fefe651f8883fd9f558fd32cb
Lincon Chondro Roy <linconchondroroy@gmail.com>
2025-11-15 12:39:39 +0600

    Car Parking App

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,15 @@
-void main(){
 
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
 }
